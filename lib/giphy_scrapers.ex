f95d0d655a1397f345d2e3127b64f911d0b7ecec
Cynthia Otieno <cynthiaawuor2000@gmail.com>
2020-01-15 13:55:22 +0300

    first commit

---
+++ lib/giphy_scrapers.ex
@@ -0,0 +1,18 @@
+defmodule GiphyScrapers do
+  @moduledoc """
+  Documentation for GiphyScrapers.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GiphyScrapers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/giphy_scrapers_test.exs
@@ -0,0 +1,8 @@
+defmodule GiphyScrapersTest do
+  use ExUnit.Case
+  doctest GiphyScrapers
+
+  test "greets the world" do
+    assert GiphyScrapers.hello() == :world
+  end
+end
